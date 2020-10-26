77261bed7d2628a41fd95e02f4785ad4b7ba5a80
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-10-25 21:30:41 -0300

    Refactor unused code

--- src/styles/themes/smarttBotDefault.ts
+++ src/styles/themes/smarttBotDefault.ts
@@ -8,5 +8,18 @@ export default {
     gray2: '#626262',
     gray3: '#333',
     gray4: '#231f20',
+    green1: '#1ba39c',
+    green2: '#00b39d',
+    background: '#f5f5f5',
+  },
+
+  media: {
+    tablet: 'only screen and min-width 769px',
+    desktop: 'only screen and min-width 1024px',
+    widescreen: 'only screen and min-width 1216px',
+    tabletOnly: 'only screen and (min-width: 769px) and (max-width: 1023px)',
+    desktopOnly: 'only screen and (min-width: 1024px) and (max-width: 1215px)',
+    widescreenOnly:
+      'only screen and (min-width: 1216px) and (max-width: 1407px)',
   },
 };
