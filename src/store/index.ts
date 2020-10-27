90042393bdd7bdc1910d5f6c8d76efc7cf3f3e36
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-10-27 08:16:12 -0300

    Add Redux Dev tools debug extension

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import reducers from './modules/rootReducer';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
