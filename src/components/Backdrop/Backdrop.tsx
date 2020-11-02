bb30913f216d51cda15e1835b79e692c80cc4712
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-11-01 21:32:08 -0300

    Refactor backdrop to render children props

--- src/components/Backdrop/Backdrop.tsx
+++ src/components/Backdrop/Backdrop.tsx
@@ -1,32 +1,36 @@
 /* eslint-disable react/require-default-props */
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import { BackdropScreen } from './styles';
 
 interface BackdropProps {
-  handleClick: () => void;
+  handleClick?: () => void;
   opacity?: number;
   visibility: boolean;
+  children?: ReactNode;
 }
 
 /**
  * Backdrop is a component that adds a dark opacity layer over the screen and provides
- * a callback to handle a click action on it.
+ * a callback to handle a click action on it. It has a z-index of 100.
  * @param handleClick Callback to handle click on Backdrop.
  * @param opacity A value ranging from 0 to 1 to control the darkness. Default is 0.5.
  * @param visibility Sets component visibility, true for visible, false for hidden.
  */
 const Backdrop: React.FC<BackdropProps> = ({
-  handleClick,
+  handleClick = () => undefined,
   opacity = 0.5,
   visibility = false,
+  children,
 }: BackdropProps) => {
   return (
     <BackdropScreen
       onClick={handleClick}
       opacity={opacity}
       visibility={visibility}
-    />
+    >
+      {children}
+    </BackdropScreen>
   );
 };
 
--- src/components/Backdrop/styles.ts
+++ src/components/Backdrop/styles.ts
@@ -9,8 +9,12 @@ export const BackdropScreen = styled.div<BackdropScreenProps>`
   position: fixed;
   width: 100vw;
   height: 100vh;
-  background: rgba(0, 0, 0, 1);
-  opacity: ${(props) => (props.visibility ? props.opacity : '0')};
+  background: rgba(0, 0, 0, 0.5);
   z-index: 100;
   visibility: ${(props) => (props.visibility ? 'visible' : 'hidden')};
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
