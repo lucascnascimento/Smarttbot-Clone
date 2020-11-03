7fac06edfa9f8209cef4c817bf73c995dccdfac1
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-11-03 14:57:48 -0300

    Add infinite scroll to RobotList

--- src/containers/RobotList/RobotList.tsx
+++ src/containers/RobotList/RobotList.tsx
@@ -1,8 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { debounce } from '@material-ui/core';
 import Robot from '../../components/Robot';
 import { useTypedSelector } from '../../store/modules/rootReducer';
-import { fetchRobotList } from '../../store/modules/robotList/asyncActions';
+import {
+  fetchRobotList,
+  refreshRobotList,
+} from '../../store/modules/robotList/asyncActions';
 import Spinner from '../../components/Spinner';
 import theme from '../../styles/themes/smarttBotDefault';
 
@@ -18,15 +22,33 @@ const RobotList: React.FC = () => {
     (state) => state.robotList.loadingRobotList
   );
   const mode = useTypedSelector((state) => state.UI.mode);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    dispatch(fetchRobotList(mode, 1));
+    dispatch(refreshRobotList(mode));
   }, [mode]);
+
+  // Load more robots when scroll to bottom
+  window.onscroll = debounce(() => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.scrollHeight
+    ) {
+      dispatch(fetchRobotList(mode, page + 1));
+      setPage(page + 1);
+    }
+  }, 100);
 
   return (
     <>
       <Container>
-        {!loadingRobots && robots.map((robot) => <Robot data={robot} />)}
+        {robots.length > 0 && (
+          <>
+            {robots.map((robot) => (
+              <Robot data={robot} key={robot.id} />
+            ))}
+          </>
+        )}
       </Container>
       {loadingRobots && <Spinner size={32} color={theme.colors.green2} />}
     </>
